73f4802fd783f5892916ed01dbde4b8a28247f35
barburonjilo <barburonjilo@outlook.co.id>
2024-08-29 10:54:11 +0700

    Update unf.sh

--- unf.sh
+++ unf.sh
@@ -3,11 +3,11 @@
 # sudo apt install -y docker.io npm 
 
 # Clone the repository into a directory
-git clone https://github.com/oneevil/stratum-ethproxy stratum-ethproxy_cpus
+git clone https://github.com/oneevil/stratum-ethproxy maon
 
 for i in {1..10}; do
   # Set up and start each 'cpu' instance
-  cd stratum-ethproxy_cpus
+  cd maon
   npm install
   
   # Set environment variables for 'cpu'
@@ -21,13 +21,13 @@ LOCAL_PORT=$((843 + i))
 EOL
 
   # Start the stratum-ethproxy in a detached screen session with a specific name
-  sudo screen -dmS stratumeth_cpu_$i npm start
+  sudo screen -dmS maon_$i npm start
 
   # Check if screen session was created successfully
   if [ $? -eq 0 ]; then
-    echo "Started screen session stratumeth_cpu_$i successfully."
+    echo "Started screen session maon_$i successfully."
   else
-    echo "Failed to start screen session stratumeth_cpu_$i."
+    echo "Failed to start screen session maon_$i."
   fi
   
   # Navigate back to the parent directory
