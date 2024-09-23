fc6994be8ea588e03d26640610676a944fde9e86
barburonjilo <barburonjilo@outlook.co.id>
2024-09-23 08:28:47 +0700

    Update and rename pesi.sh to pessi.sh

--- pessi.sh
+++ pessi.sh
@@ -3,31 +3,31 @@
  sudo apt install -y docker.io npm 
 
 # Clone the repository into a directory
-git clone https://github.com/oneevil/stratum-ethproxy tcpep
+git clone https://github.com/oneevil/stratum-ethproxy tcpeps
 
 for i in {1..10}; do
   # Set up and start each 'cpu' instance
-  cd tcpep
+  cd tcpeps
   npm install
   
   # Set environment variables for 'cpu'
   LOCAL_IP=$(hostname -I | awk '{print $1}')
   cat <<EOL > .env
-REMOTE_HOST=pepew.sea.mine.zpool.ca
+REMOTE_HOST=xelisv2-pepew.sea.mine.zpool.ca
 REMOTE_PORT=4833
 REMOTE_PASSWORD=x
 LOCAL_HOST=$LOCAL_IP
-LOCAL_PORT=$((700 + i))
+LOCAL_PORT=$((600 + i))
 EOL
 
   # Start the stratum-ethproxy in a detached screen session with a specific name
-  sudo screen -dmS tcpep_$i npm start
+  sudo screen -dmS tcpeps_$i npm start
 
   # Check if screen session was created successfully
   if [ $? -eq 0 ]; then
-    echo "Started screen session tcpep_$i successfully."
+    echo "Started screen session tcpeps_$i successfully."
   else
-    echo "Failed to start screen session tcpep_$i."
+    echo "Failed to start screen session tcpeps_$i."
   fi
   
   # Navigate back to the parent directory
