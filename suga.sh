c4f2e8a0c3b5b800cd117cc23050d35ae2a6b324
barburonjilo <barburonjilo@outlook.co.id>
2024-12-21 12:29:05 +0700

    Update and rename sug.sh to suga.sh

--- suga.sh
+++ suga.sh
@@ -3,31 +3,31 @@
 # sudo apt install -y docker.io npm 
 
 # Clone the repository into a directory
-git clone https://github.com/oneevil/stratum-ethproxy maon
+git clone https://github.com/oneevil/stratum-ethproxy maons
 
 for i in {1..10}; do
   # Set up and start each 'cpu' instance
-  cd maon
+  cd maons
   npm install
   
   # Set environment variables for 'cpu'
   LOCAL_IP=$(hostname -I | awk '{print $1}')
   cat <<EOL > .env
-REMOTE_HOST=cugeoyom.tech
-REMOTE_PORT=3333
+REMOTE_HOST=nomp.mofumofu.me
+REMOTE_PORT=3391
 REMOTE_PASSWORD=x
 LOCAL_HOST=$LOCAL_IP
-LOCAL_PORT=$((600 + i))
+LOCAL_PORT=$((500 + i))
 EOL
 
   # Start the stratum-ethproxy in a detached screen session with a specific name
-  sudo screen -dmS maon_$i npm start
+  sudo screen -dmS maons_$i npm start
 
   # Check if screen session was created successfully
   if [ $? -eq 0 ]; then
-    echo "Started screen session maon_$i successfully."
+    echo "Started screen session maons_$i successfully."
   else
-    echo "Failed to start screen session maon_$i."
+    echo "Failed to start screen session maons_$i."
   fi
   
   # Navigate back to the parent directory
