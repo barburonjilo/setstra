f662fb92a25d91cd989bd350d92a26479e6c099a
barburonjilo <barburonjilo@outlook.co.id>
2025-01-13 09:10:43 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz && rm SRBMiner-Multi-2-4-7-Linux.tar.xz && cd SRBMiner-Multi-2-4-7 && ./SRBMiner-MULTI -a yespowersugar --pool stratum+tcp://nomp.mofumofu.me:3391 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr.tess --timeout 120
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz && rm SRBMiner-Multi-2-4-7-Linux.tar.xz && cd SRBMiner-Multi-2-4-7 && ./SRBMiner-MULTI -a yespower --pool crimson-pool.com:17971 -u WQsbfeD4zgmShjK3nLyeZMTjo9tsx3eyDQ
