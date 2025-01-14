74d1f9ac3775446746fe7d25bcc48d8e254421ee
barburonjilo <barburonjilo@outlook.co.id>
2025-01-14 19:02:27 +0700

    Update nip.sh

--- nip.sh
+++ nip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a mike -o stratum+tcp://connect.fortunepool.xyz:3053 -u FfVFHL7VBstZaZodRTFqAC3QGUpbuAcutM -p x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yespowersugar  -o stratum+tcp://nomp.mofumofu.me:3391 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr.tess -p x
