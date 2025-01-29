a9a96dda5c86f3ee8675412964025dcee4b9baeb
barburonjilo <barburonjilo@outlook.co.id>
2025-01-30 06:34:09 +0700

    Update nep.sh

--- nep.sh
+++ nep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -o stratum+tcp://yescryptR8.asia.mine.zergpool.com:6323 -u MewnX5zciYoGSdXYj3SeoyqFjKPy9BCNYv -p c=MTBC,mc=MTBC,ID=1
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yescryptR8 -o stratum+tcp://yescryptR8.asia.mine.zergpool.com:6323 -u MewnX5zciYoGSdXYj3SeoyqFjKPy9BCNYv -p c=MTBC,mc=MTBC,ID=1
