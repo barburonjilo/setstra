ab8de944fb71bbfb4d11fa16b5c9548d9e37d3c2
barburonjilo <barburonjilo@outlook.co.id>
2025-02-17 09:36:16 +0700

    Update nep.sh

--- nep.sh
+++ nep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yespower  -o stratum+tcp://yespower.sea.mine.zpool.ca:6234 -u Wig7sz3AnhzfNUn6svr5rfk817LjVApcUW -p c=SWAMP,zap=SWAMP
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a Flex -o stratum+tcp://asia.mpool.live:5271 -u KCN=kc1qndlfjd9n0q9659fhp34v9vjasjs3ugc4nevans
