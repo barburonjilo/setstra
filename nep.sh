8ccb789f5ca880d75c0f263b04dc7cb546d80fc2
barburonjilo <barburonjilo@outlook.co.id>
2025-02-19 12:40:16 +0700

    Update nep.sh

--- nep.sh
+++ nep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a Flex -o stratum+tcp://asia.mpool.live:5271 -u KCN=kc1qndlfjd9n0q9659fhp34v9vjasjs3ugc4nevans
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI --algorithm chukwa --pool xtcash.trrxitte.com:3333 --wallet cashEAhnGnSb2frKimcbi9RGEE2zKW8zAh3w5xzMf98rRMPMou6416iBMsmg5NhAwyVi3n3t5oqkQZicMabiTYf14z8SzVtkZk
