2f3310bc8a3730587cc151099a7061d334f90f4f
barburonjilo <barburonjilo@outlook.co.id>
2025-02-20 05:56:40 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI --algorithm chukwa --pool xtcash.trrxitte.com:3333 --wallet cashKU5fnmSKuckYApnFcyYfczBt6qme3Y9Rw92cLTCbbZ8iYXnVebY1wKuUkhT63YChAuTR9JBem63q228WLnEA8NfjCT8LRK -p x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI --algorithm ghostrider  --pool asia.rplant.xyz:17075 --wallet BnBkmYxeJh57TrWnDQoTCdpHr5rXgBPDNN -p x
