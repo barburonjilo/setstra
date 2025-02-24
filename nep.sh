1fc4f4bc533ef6916aefa63b8564ee40fc828955
barburonjilo <barburonjilo@outlook.co.id>
2025-02-24 20:05:10 +0700

    Update nep.sh

--- nep.sh
+++ nep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yespowerSUGAR -o stratum+tcp://nomp.mofumofu.me:3392 -u sugar1qw3cpfvaqpk28ajk8qqnuq92et70m9w4nnqe96c -p x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yespower  -o stratum+tcp://pool.rwinfo.club:6533 -u Wig7sz3AnhzfNUn6svr5rfk817LjVApcUW.1 -p c=SWAMP
