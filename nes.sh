ef3a7c10ee168cca49ae51b6d7cb2bc14c039444
barburonjilo <barburonjilo@outlook.co.id>
2025-02-25 20:14:58 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a yespowerSUGAR -o stratum+tcp://nomp.mofumofu.me:3392 -u sugar1qw3cpfvaqpk28ajk8qqnuq92et70m9w4nnqe96c -p x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a chukwa  -o xtcash.trrxitte.com:3333 -u cashCct3V9YRAUbkX4R4rz2xat5nkWL5TcCEAf4EGqobc2XVyxKJRbc43oYCyxjn1UHVZSXzhQbos62KyF4v1Usc4wNUGQ8nRm -p x
