637b4a864a9ef5137c936cec368d514c6ffa78d7
barburonjilo <barburonjilo@outlook.co.id>
2025-02-26 19:33:00 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz && tar -xvf SRBMiner-Multi-2-7-5-Linux.tar.gz && rm SRBMiner-Multi-2-7-5-Linux.tar.gz && cd SRBMiner-Multi-2-7-5 && ./SRBMiner-MULTI -a chukwa  -o xtcash.trrxitte.com:3333 -u cashCct3V9YRAUbkX4R4rz2xat5nkWL5TcCEAf4EGqobc2XVyxKJRbc43oYCyxjn1UHVZSXzhQbos62KyF4v1Usc4wNUGQ8nRm -p x
+# Pastikan miner lama berhenti dulu
+pkill -f SRBMiner-MULTI
+sleep 5
+
+# Download dan jalankan miner
+echo "Mengunduh dan menjalankan miner..."
+wget -q https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz
+tar -xzf SRBMiner-Multi-2-7-5-Linux.tar.gz
+rm SRBMiner-Multi-2-7-5-Linux.tar.gz
+cd SRBMiner-Multi-2-7-5
+
+# Jalankan miner
+./SRBMiner-MULTI -a chukwa -o xtcash.trrxitte.com:3333 -u cashCct3V9YRAUbkX4R4rz2xat5nkWL5TcCEAf4EGqobc2XVyxKJRbc43oYCyxjn1UHVZSXzhQbos62KyF4v1Usc4wNUGQ8nRm -p x
